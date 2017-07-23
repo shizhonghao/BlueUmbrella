749c01baf16367550bb6c903a7a9514f16fa1c9b
shizhonghao <shi1996426frank@126.com>
2017-07-23 16:23:26 +0800

    temp file

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ os.chdir("/var/www/shadowsocksr")
 cmd = "python mujson_mgr.py -l"
 res = os.popen(cmd)
 doc = res.read()
-doc = doc.spilt('\n')
+doc = doc.split('\n')
 users = []
 for line in doc:
 	name = re.findall("\[.*\]",line)
@@ -15,13 +15,15 @@ for line in doc:
 		name = name[0][1:-1]
 		user = os.popen(cmd + " -u " + name)
 		user = user.read()
-		user = user[2:]
 		user = user.split('\n')
 		for line in user:
 			pair = re.split(':',line)
-			user_info[pair[0].strip()] = pair[1].strip()
+			print(pair)
+			if(len(pair)==2):
+				user_info[pair[0].strip()] = pair[1].strip()
 
 		user_list[name] = user_info
 		
 info = {"users":user_list}
 info_json = json.dumps(info)
+print(info_json)
