55d6aad67d02ff5485a3169bc67e3f429ba17071
shizhonghao <shi1996426frank@126.com>
2017-07-23 16:27:29 +0800

    get all users

--- App/controller.py
+++ App/controller.py
@@ -1,21 +1,49 @@
 from flask_restful import Resource, fields, marshal_with, Api, reqparse
-from flask import jsonify, make_response, abort, request, g, url_for, current_app
-from flask_login import current_user
-from App.auth import login_required
+from flask_security import auth_token_required, roles_required, login_user, login_required
+from flask import jsonify, make_response, abort, request, g, url_for
+from flask_security.utils import encrypt_password, verify_password
+
+import os
+import re
+import json
 
 class Users(Resource):
-    @login_required(admin=True)
+    @login_required
+    @auth_token_required
+    @roles_required('admin')
     def get(self):
-        # if not current_user.is_authenticated:
-        #     return current_app.login_manager.unauthorized()
-        # if not current_user.is_admin:
-        #     return current_app.login_manager.unauthorized()     
-        return jsonify(Status="allright")
+    	user_list = {}
+		os.chdir("/var/www/shadowsocksr")
+		cmd = "python mujson_mgr.py -l"
+		res = os.popen(cmd)
+		doc = res.read()
+		doc = doc.split('\n')
+		users = []
+		for line in doc:
+			name = re.findall("\[.*\]",line)
+			if(name):#list is not empty
+				user_info = {}
+				name = name[0][1:-1]
+				user = os.popen(cmd + " -u " + name)
+				user = user.read()
+				user = user.split('\n')
+				for line in user:
+					pair = re.split(':',line)
+					print(pair)
+					if(len(pair)==2):
+						user_info[pair[0].strip()] = pair[1].strip()
+
+				user_list[name] = user_info
+				
+		info = {"users":user_list}
+		info_json = json.dumps(info)
+		print(info_json)
+    	return info_json
 
 class Serverinfo(Resource):
-    @login_required()
+    @login_required
     def get(self):
-        return jsonify(Status="allright")
+        pass
 
 # class Login(Resource):
 #     def post(self):
@@ -29,3 +57,5 @@ class Serverinfo(Resource):
 #             return {"message": "登陆成功", "token": user.get_auth_token()}, 200
 #         else:
 #             return {"message": "用户名或密码错误"}, 401
+
+
