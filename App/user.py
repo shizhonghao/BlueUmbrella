31c9c81c5475f6ff71fc5742a65258d30a63315b
shizhonghao <shi1996426frank@126.com>
2017-07-24 00:06:12 +0800

    read info

--- App/user.py
+++ App/user.py
@@ -1,9 +1,38 @@
+import json
+
+def change_key_name(dic,key_old,key_new):
+	dic[key_new] = dic.pop(key_old)
+
+def get_mongodb_info(dic):
+	pass
 
 def get_all_users():
-    pass
+	f = open("/var/www/shadowsocksr/mudb.json","r")
+	data = json.load(f)
+	user_info = {}
+	for line in data:
+		change_key_name(line,"d","downward_transfer")
+		change_key_name(line,"u","upward_transfer")
+		change_key_name(line,"passwd","password")
+		line.pop("transfer_enable")
+		change_key_name(line,"enable","transfer_enable")
+		get_mongodb_info(line)
+		user_info[line["user"]] = line
+	info = {"users":user_info}
+    return info
 
 def get_user(username):
-    pass
+	f = open("/var/www/shadowsocksr/mudb.json","r")
+	data = json.load(f)
+	for line in data:
+		if(line["user"] == username):
+			change_key_name(line,"d","downward_transfer")
+			change_key_name(line,"u","upward_transfer")
+			change_key_name(line,"passwd","password")
+			line.pop("transfer_enable")
+			change_key_name(line,"enable","transfer_enable")
+			info = line
+	return info
 
 def modify_user(username):
     pass
