abb77f8bbd34c1606a8a533b90b6e192cea59a2b
shizhonghao <shi1996426frank@126.com>
2017-08-01 23:17:07 +0800

    add qr-code

--- front-end/src/components/View.vue
+++ front-end/src/components/View.vue
@@ -11,105 +11,181 @@
           {{ key }} ,{{ val }}:&nbsp;
         </div>
       </el-col>-->
+      <el-tabs type="border-card" >
+        <el-tab-pane label="用户信息" name="first">
 
-      <el-col :span="4" :offset="8" >
-        <div class="key">
-          用户:&nbsp;
-        </div>
-      </el-col>
-      <el-col :span="4" :offset="0" >
-        <div class="val">
-          {{ current_user }}
-        </div>
-      </el-col>
-      <el-row>
-      </el-row>
-
-      <el-col :span="4" :offset="8" >
-        <div class="key">
-          ss密码:&nbsp;
-        </div>
-      </el-col>
-      <el-col :span="4" :offset="0" >
-        <div class="val">
-          {{ ss_password }}
-        </div>
-      </el-col>
-      <el-row>
-      </el-row>
-
-      <el-col :span="4" :offset="8" >
-        <div class="key">
-          port:&nbsp;
-        </div>
-      </el-col>
-      <el-col :span="4" :offset="0" >
-        <div class="val">
-          {{ port }}
-        </div>
-      </el-col>
-      <el-row>
-      </el-row>
+          <el-row type="flex" class="row-bg">
+            <el-col :span="4" offset="4">
+              <div class="key">
+                用户:&nbsp;
+              </div>
+            </el-col>
+            <el-col :span="4">
+              <div class="val">
+                {{ current_user }}
+              </div>
+            </el-col>
+          </el-row>
 
 
-      <el-col :span="4" :offset="10">
-        <el-button @click="$router.push('/logout')" type="primary">登出</el-button>
-      </el-col>
+          <el-row type="flex" class="row-bg">
+            <el-col :span="4" offset="4">
+              <div class="key">
+                ss密码:&nbsp;
+              </div>
+            </el-col>
+            <el-col :span="4">
+              <div class="val">
+                {{ ss_password }}
+              </div>
+            </el-col>
+          </el-row>
+
+
+          <el-row type="flex" class="row-bg">
+            <el-col :span="4" offset="4">
+              <div class="key">
+                enable:&nbsp;
+              </div>
+            </el-col>
+            <el-col :span="4">
+              <div class="val">
+                {{ enable }}
+              </div>
+            </el-col>
+          </el-row>
+
+
+          <el-row type="flex" class="row-bg">
+
+            <el-col :span="4" offset="4">
+              <div class="key">
+                protocol:&nbsp;
+              </div>
+            </el-col>
+            <el-col :span="4">
+              <div class="val">
+                {{ protocol }}
+              </div>
+            </el-col>
+          </el-row>
+
+
+          <el-row type="flex" class="row-bg">
+            <el-col :span="4" offset="4">
+              <div class="key">
+                method:&nbsp;
+              </div>
+            </el-col>
+            <el-col :span="4">
+              <div class="val">
+                {{ method }}
+              </div>
+            </el-col>
+          </el-row>
+
+
+          <el-row type="flex" class="row-bg">
+
+            <el-col :span="4" offset="4">
+              <div class="key">
+                obfs:&nbsp;
+              </div>
+            </el-col>
+            <el-col :span="4">
+              <div class="val">
+                {{ obfs }}
+              </div>
+            </el-col>
+            <el-row>
+            </el-row>
+          </el-row>
+
+
+          <el-col :span="4" :offset="10">
+            <el-button @click="$router.push('/logout')" type="primary">登出</el-button>
+          </el-col>
+
+        </el-tab-pane>
+
+
+        <el-tab-pane label="SSR二维码" name="second">
+          <vue-qr :bgSrc='src' :logoSrc="src2" text="这是一个测试界面" height="200" width="200"></vue-qr>
+          <el-col :span="4" :offset="10">
+            <el-button @click="$router.push('/logout')" type="primary">登出</el-button>
+          </el-col>
+        </el-tab-pane>
+
+      </el-tabs>
+
 
     </div>
   </div>
 </template>
 
 <script>
+  import VueQr from 'vue-qr'
 
   import ElCol from "element-ui/packages/col/src/col";
-export default {
-  components: {ElCol},
-  name: 'view',
-  data () {
-    return {
-    }
-  },
-  computed:{
-    current_user(){
-      return sessionStorage.getItem("username")
-    },
-    ss_password(){
-        return JSON.parse(sessionStorage.getItem("userinfo")).ss_password
-    },
-    port(){
-      return JSON.parse(sessionStorage.getItem("userinfo")).port
-    },
-    tableData(){
-      return [{
-        key:"username",
-        value: "ray",
-        description: "您的用户名"
-      } , {
-        key:"ss_password",
-        value:"123",
-        description: "您的shadowsocks密码"
+  export default {
+    components: {ElCol,VueQr},
+    name: 'view',
+    data () {
+      return {
       }
-      ]
-    }
-  },
-  methods: {
-  },
-  created(){
-    this.$ajax.get('/users/'.concat(sessionStorage.getItem("username")))
-    .then((response) => {
-      sessionStorage.setItem("userinfo", JSON.stringify(response.data))
-    })
-    .catch((error) =>{
-      console.log(error)
-    })
-  },
-  beforeCreate(){
-    if(!(sessionStorage.getItem("state") && JSON.parse(sessionStorage.getItem("state")))){
-      this.$router.push('/login')
+    },
+    computed:{
+      current_user(){
+        return sessionStorage.getItem("username")
+      },
+      ss_password(){
+        return JSON.parse(sessionStorage.getItem("userinfo")).ss_password
+      },
+      port(){
+        return JSON.parse(sessionStorage.getItem("userinfo")).port
+      },
+      method(){
+        return JSON.parse(sessionStorage.getItem("userinfo")).method
+      },
+      protocol(){
+        return JSON.parse(sessionStorage.getItem("userinfo")).protocol
+      },
+      obfs(){
+        return JSON.parse(sessionStorage.getItem("userinfo")).obfs
+      },
+      enable(){
+        return JSON.parse(sessionStorage.getItem("userinfo")).enable
+      },
+      tableData(){
+        return [{
+          key:"username",
+          value: "ray",
+          description: "您的用户名"
+        } , {
+          key:"ss_password",
+          value:"123",
+          description: "您的shadowsocks密码"
+        }
+        ]
+      }
+    },
+    methods: {
+    },
+    created(){
+      this.$ajax.get('/users/'.concat(sessionStorage.getItem("username")))
+        .then((response) => {
+          sessionStorage.setItem("userinfo", JSON.stringify(response.data))
+        })
+        .catch((error) =>{
+          console.log(error)
+        })
+    },
+    beforeCreate(){
+      if(!(sessionStorage.getItem("state") && JSON.parse(sessionStorage.getItem("state")))){
+        this.$router.push('/login')
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -140,16 +216,27 @@ export default {
     padding: 0;
   }
   .el-row {
-    margin-bottom: 20px;
-    &:last-child {
-      margin-bottom: 0;
-    }
+    margin-bottom: 10px;
+  &:last-child {
+     margin-bottom: 0;
+   }
   }
   .el-col {
     border-radius: 4px;
   }
   .row-bg {
-    padding: 10px 0;
+    padding: 5px 0;
     background-color: #f9fafc;
+  }
+
+  .text {
+    font-size: 14px;
+  }
+  .item {
+    padding: 18px 0;
+  }
+  .el-tabs {
+    width: 480px;
+    margin: auto;
   }
 </style>
