20c7b43770c144b61ee86c877e09041ac31abcab
Raycursive <m@raycursive.com>
2017-08-24 20:44:20 +0800

    upload the part of admin tools

---
+++ admin/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  created(){
+    this.$ajax.get('/login')
+    .then((response) => {
+      sessionStorage.setItem("state", response.data.LoggedIn)
+      if(JSON.parse(sessionStorage.getItem("state"))){
+        this.$router.push('/view')
+      } else {
+        this.$router.push('/login')
+      }
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+}
+</script>
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
