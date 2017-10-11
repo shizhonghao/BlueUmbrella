517183c9d9ef487a2162bfa83f4fafc354704f76
shizhonghao <shi1996426frank@126.com>
2017-10-11 20:25:14 +0800

    monthly reset added

--- db/monthly_reset.py
+++ db/monthly_reset.py
@@ -1,11 +1,11 @@
 import json
 
-with open("/var/www/shadowsocksr/mudb.json","r"):
+with open("/var/www/shadowsocksr/mudb.json","r") as f:
     json_data = json.load(f)
 for index, line in enumerate(json_data):
     line["u"] = 0
     line["d"] = 0
 
-with open("/var/www/shadowsocksr/mudb.json","w"):
+with open("/var/www/shadowsocksr/mudb.json","w") as f:
     json.dump(json_data,f,sort_keys = True,indent = 4)
     print("data reseted")
