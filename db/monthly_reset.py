0b52957fc78cdebce26f51ba6789b4932579ad3a
shizhonghao <shi1996426frank@126.com>
2017-10-12 11:40:05 +0800

    monthly backup

--- db/monthly_reset.py
+++ db/monthly_reset.py
@@ -1,7 +1,15 @@
-# Edit /etc/crontab to set time interval of resetting data.
+# edit /etc/crontab to set time interval of resetting data.
 
 import json
+import os
+import datetime
 
+# backup database before reset
+t = datetime.date.today()
+copy_path = "/var/www/shadowsocksr/mudb-backup-" + str(t) + ".json"
+os.system("cp /var/www/shadowsocksr/mudb.json " + copy_path)
+
+# reset dataflow to 0
 with open("/var/www/shadowsocksr/mudb.json","r") as f:
     json_data = json.load(f)
 for index, line in enumerate(json_data):
