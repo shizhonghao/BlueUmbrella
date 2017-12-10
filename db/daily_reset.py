f0846c59587b3f6be18c33ebe392b50c45fe5c35
shizhonghao <shi1996426frank@126.com>
2017-12-10 21:34:09 +0800

    bug fix

--- db/daily_reset.py
+++ db/daily_reset.py
@@ -20,19 +20,19 @@ def disable_user(username):
         json.dump(json_data,f,sort_keys = True,indent = 4)
 
 for user in user_dat:
-    if days_remaining in user:
+    if "days_remaining" in user:
         days_rem = user["days_remaining"]
         if days_rem == 1:
             disable_user(user["username"])
-            collection.update({"username":user["username"]}, {"$set":{"days_remaining":0}}
+            collection.update({"username":user["username"]}, {"$set":{"days_remaining":0}})
         elif days_rem > 1:
-            collection.update({"username":user["username"]}, {"$set":{"days_remaining":user[days_remaining]-1}}
+            collection.update({"username":user["username"]}, {"$set":{"days_remaining":user[days_remaining]-1}})
         else:
             pass
 
     # if field wasn't set, init days_remaining to 7d
     else:
-        collection.update({"username":user["username"]}, {"$set":{"days_remaining":7}}
+        collection.update({"username":user["username"]}, {"$set":{"days_remaining":7}})
     #print(user["username"])
 
 
